c03ab3a3c54b81dca2654e4705aa9b963d7e71d6
Raval Chetan <126232457+chetan382000@users.noreply.github.com>
2024-03-17 15:38:24 +0530

    Update user.js
    new changes

--- server/router/user.js
+++ server/router/user.js
@@ -29,3 +29,25 @@ Router.route('/upload')
 
  
 module.exports = Router;
+
+// const multer = require('multer')
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "uploads/");
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, Date.now() + file.originalname);
+//     },
+//   });
+  
+//   const upload = multer({ storage: storage });
+
+//   module.exports = upload;
+
+
+  // another file setup
+
+//   const upload = require('./imageUpload')
+
+// route.post("/create",upload.single("image"),
